d959904a4f0bb816f1491681a1390ef32a14de03
Yulianaa7 <yuliananurhanifah77@gmail.com>
2021-02-26 15:18:44 +0700

    Enkapsulasi Latihan 1

---
+++ src/ujibus/Bus.java
@@ -0,0 +1,18 @@
+package ujibus;
+
+/**
+ *
+ * @author Yulia
+ */
+public class Bus {
+    //mendaklarasikan variabel 
+    public int penumpang;
+    public int maxPenumpang;
+    
+    //method void
+    public void cetak(){
+        System.out.println("Penumpang Bus sekarang adalah "+ penumpang);
+        System.out.println("Penumpang maksimun seharusnya adalah "+ maxPenumpang);
+    }
+}
+
---
+++ src/ujibus/UjiBus.java
@@ -0,0 +1,34 @@
+package ujibus;
+
+/**
+ *
+ * @author Yuliana
+ */
+public class UjiBus {
+
+    public static void main(String[] args) {
+        //membuat objek busMini dari kelas Bus
+        Bus busMini = new Bus();
+        //memasukkan nilai jumlah penumpang dan penumpang maksimal ke dalam objek busMini
+        busMini.penumpang = 5;
+        busMini.maxPenumpang = 15;
+        //memanggil method cetak pada kelas Bus
+        busMini.cetak();
+        
+        //menambahkan penumpang pada busMini
+        busMini.penumpang = busMini.penumpang + 5;
+        //memanggil method cetak pada kelas Bus
+        busMini.cetak();
+        
+        //mengurangi jumlah penumpang pada busMini
+        busMini.penumpang = busMini.penumpang - 2;
+        //memanggil method cetak pada kelas Bus
+        busMini.cetak();
+        
+        //menambahkan jumlah penumpang pada busMini
+        busMini.penumpang = busMini.penumpang + 8;
+        //memanggil method cetak pada kelas Bus
+        busMini.cetak();
+    }
+    
+}
